046287e758f90a19ae8eb91e55cfad478c261808
AeroX <aeroxdevs@gmail.com>
2026-04-22 17:46:15 +0530

    Initial commit: add src/commands/leaderboard/invites.js

---
+++ src/commands/leaderboard/invites.js
@@ -0,0 +1,40 @@
+// Falcron | AeroX Development
+// Author: itsfizys
+import { Command } from '#command';
+import { MessageFlags } from 'discord.js';
+import { db } from '#dbManager';
+import { buildInviteLeaderboard, PER_PAGE } from '#utils';
+
+class InvitesLeaderboardCommand extends Command {
+        constructor() {
+                super({
+                        name: 'invitesleaderboard',
+                        description: 'Shows the top inviters in this server',
+                        aliases: [],
+                        cooldown: 5,
+                        enabledSlash: false,
+                });
+        }
+
+        async execute({ ctx }) {
+                const allCounts = await db.userInviteCounter?.getAllByGuild(ctx.guild.id) ?? [];
+                allCounts.sort((a, b) => (b.total || 0) - (a.total || 0));
+
+                const totalPages = Math.max(1, Math.ceil(allCounts.length / PER_PAGE));
+                const { components } = buildInviteLeaderboard(allCounts, 1, totalPages, ctx.guild.id, ctx.user.id);
+
+                await ctx.reply({ components, flags: MessageFlags.IsComponentsV2 });
+        }
+}
+
+export default new InvitesLeaderboardCommand();
+
+/**
+ * Project: Falcron
+ * Author: itsfizys (Aegis)
+ * Organization: AeroX Development
+ * GitHub: https://github.com/AeroXDevs
+ * License: Custom
+ *
+ * © 2026 AeroX Development. All rights reserved.
+ */
